b6c52656b0c95c1512f84aa8da80a959d747de46
ameCont <americancont7@gmail.com>
2020-05-26 13:41:46 +0300

    smsapp v0

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+const Nexmo = require('nexmo');
+const socketio = require('socket.io');
+
+//Init
+const app = express();
+
+//Template Engine Setup
+app.set('view engine','html');
+app.engine('html',ejs.renderFile);
+
+//Public folder setup
+app.use(express.static(__dirname + '/public'));
+
+//Body parser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//Index route
+app.get('/',(req,res) => {
+    res.render('index');
+});
+
+//Define port
+const port = 4000;
+
+//Start server
+const server = app.listen(port, () => console.log(`Server started on port ${port}`));
